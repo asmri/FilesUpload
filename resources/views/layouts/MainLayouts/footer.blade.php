cf24bd12e8d547458aeebcf1a1bec7b76a3081d6
asmri <soalasmri@gmail.com>
2023-04-01 11:53:51 +0300

    Front End

---
+++ resources/views/layouts/MainLayouts/footer.blade.php
@@ -0,0 +1,3 @@
+
+    <p>This is my Header content.</p>
+
---
+++ resources/views/layouts/MainLayouts/main.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+
+<body
+<div class="container-fluid mx-auto relative overflow-hidden">
+    <img alt="" src="https://salient.tailwindui.com/_next/static/media/background-faqs.55d2e36a.jpg" width="1558" height="946" decoding="async" data-nimg="1" class="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]" loading="lazy" style="color:transparent">
+    @yield('content')
+
+</div>
+</body>
+</html>
+
+
